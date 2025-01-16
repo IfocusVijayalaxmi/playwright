7751c43dfde9294b19c72b07e66187d26f1866ad
vijayalaxmi-byakod <byakodvijayalaxmi@gmail.com>
2025-01-16 11:48:24 +0530

    recent commit

--- tests/kitchen1.spec.js
+++ tests/kitchen1.spec.js
@@ -2,7 +2,7 @@ import {test,expect} from '@playwright/test'
 
 test('kitchenPage', async({page})=>
 {
-    await page.pause()
+    
     await page.goto('https://kitchen.applitools.com/')
     if(await expect(page.locator('text=The Kitchen')).toBeVisible())
         await page.locator('text=The Kitchen').click()
